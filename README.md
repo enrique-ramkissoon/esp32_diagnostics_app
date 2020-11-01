8fba19742ecd008b072d18b088cb1f28141f861c
Enrique Ramkissoon <enrique.ramkissoon@gmail.com>
2020-10-31 21:16:27 -0400

    First Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# esp32_diagnostics_app
+
---
+++ android/app/src/main/kotlin/com/example/esp32_diagnostics_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.esp32_diagnostics_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
