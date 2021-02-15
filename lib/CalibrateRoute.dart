c593377baae9441c8f72396d74f5eebdf242ad8e
Enrique Ramkissoon <enrique.ramkissoon@gmail.com>
2021-02-15 05:44:48 -0400

    Send Calibrated Sensitivity to BLE Server

--- lib/CalibrateRoute.dart
+++ lib/CalibrateRoute.dart
@@ -21,13 +21,15 @@ class CalibrateState extends State<CalibrateRoute>{
 
   double calibrationFactor = -1;
 
-  double lastMass = -1;
-  double lastCalFac = -1;
-  double lastAdc = -1;
+  // double lastMass = -1;
+  // double lastCalFac = -1;
+  // double lastAdc = -1;
   
   String lastAdcStr = '-1';
   String lastMassStr = '-1';
   String lastCalFacStr = '-1';
+  
+  bool running = false;
 
   @override
   void initState(){
@@ -66,6 +68,11 @@ class CalibrateState extends State<CalibrateRoute>{
                 child: Text('View Calibration Statistics', style: TextStyle(color: Colors.white)),
                 color: Colors.green[600],
                 onPressed: (){
+
+                  setState((){
+                    running = true;
+                  });
+
                   startReadTask();
                 }
               ),
@@ -75,31 +82,72 @@ class CalibrateState extends State<CalibrateRoute>{
                 color: Colors.green[600],
                 onPressed: () async {
 
-                  await step1();
-                  
-                  List<int> bleRead1 = await this.widget.characteristics.rc.read();
-
-                  String readStr1 = new String.fromCharCodes(bleRead1);
-                  String fixedStr1 = readStr1.substring(0,readStr1.indexOf(String.fromCharCode(0)));
-                  print("Str1 " + fixedStr1);
-
-                  int adcOut1 = int.parse(fixedStr1.trim());
-
-                  await step2();
-
-                  List<int> bleRead2 = await this.widget.characteristics.rc.read();
-                  String readStr2 = new String.fromCharCodes(bleRead2);
-                  String fixedStr2 = readStr2.substring(0,readStr2.indexOf(String.fromCharCode(0)));
-
-                  int adcOut2 = int.parse(fixedStr2.trim());
-
                   String sm1 = sm1Controller.text;
-                  String sm2 = sm2Controller.text;
+                  List<String> standardMassesStr = sm1.split(",");
+                  List<double> stdMasses = new List(standardMassesStr.length);
+                  List<double> stdAdc = new List(stdMasses.length);
+                  List<double> sensitivities = new List(stdMasses.length -1);
 
-                  double sm1d = double.parse(sm1);
-                  double sm2d = double.parse(sm2);
+                  for(int i=0;i<standardMassesStr.length;i++){
+                    stdMasses[i] = double.parse(standardMassesStr[i]);
+                  }
 
-                  calibrationFactor = (sm2d - sm1d) / (adcOut2.toDouble() - adcOut1.toDouble());
+                  for(int n = 0;n<stdMasses.length;n++){
+                    await stepN(n);
+
+                    int adcOut = int.parse(lastAdcStr);
+                    await new Future.delayed(const Duration(milliseconds : 200));
+                    int adcOut2 = int.parse(lastAdcStr);
+
+                    double adcAvg = (adcOut2.toDouble() + adcOut.toDouble())/2;
+
+                    stdAdc[n] = adcAvg;
+                  }
+
+                  for(int i = 0;i < (stdMasses.length -1) ;i++){
+                    double y1 = stdMasses[i];
+                    double y2 = stdMasses[i+1];
+                    double x1 = stdAdc[i];
+                    double x2 = stdAdc[i+1];
+
+                    double gradient = (y2-y1)/(x2-x1);
+
+                    sensitivities[i] = gradient;
+                  }
+
+                  // await step1();
+                  
+                  // List<int> bleRead1 = await this.widget.characteristics.rc.read();
+
+                  // String readStr1 = new String.fromCharCodes(bleRead1);
+                  // String fixedStr1 = readStr1.substring(0,readStr1.indexOf(String.fromCharCode(0)));
+                  // print("Str1 " + fixedStr1);
+
+                  // int adcOut1 = int.parse(fixedStr1.trim());
+
+                  // await step2();
+
+                  // List<int> bleRead2 = await this.widget.characteristics.rc.read();
+                  // String readStr2 = new String.fromCharCodes(bleRead2);
+                  // String fixedStr2 = readStr2.substring(0,readStr2.indexOf(String.fromCharCode(0)));
+
+                  // int adcOut2 = int.parse(fixedStr2.trim());
+
+                  // String sm1 = sm1Controller.text;
+                  // String sm2 = sm2Controller.text;
+
+                  // double sm1d = double.parse(sm1);
+                  // double sm2d = double.parse(sm2);
+
+                  // calibrationFactor = (sm2d - sm1d) / (adcOut2.toDouble() - adcOut1.toDouble());
+
+                  double sensSum = 0;
+
+                  for(int i = 0;i < sensitivities.length;i++){
+                    sensSum = sensSum + sensitivities[i];
+                  }
+
+                  calibrationFactor = sensSum/(sensitivities.length);
 
                   await step3(calibrationFactor);
 
@@ -110,7 +158,17 @@ class CalibrateState extends State<CalibrateRoute>{
 
                     List<int> cfChars = utf8.encode(cfStr);
 
-                    this.widget.characteristics.write(cfChars);
+                    setState((){
+                      running = false;
+                    });
+
+                    await new Future.delayed(const Duration(milliseconds : 1000));
+
+                    await this.widget.characteristics.write(cfChars);
+
+                    setState(() {
+                      running = true;
+                    });
                   }
                 },
               ),
@@ -126,6 +184,33 @@ class CalibrateState extends State<CalibrateRoute>{
     );
   }
 
+  Future<void> stepN(int n) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: Colors.green[800],
+          title: Text('Step ' + n.toString(), style: TextStyle(color: Colors.white)),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text('Place Standard Mass ' + n.toString() + ' on the Scale', style: TextStyle(color: Colors.white)),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Next', style: TextStyle(color: Colors.white)),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   Future<void> step1() async {
     return showDialog<void>(
@@ -226,19 +311,25 @@ class CalibrateState extends State<CalibrateRoute>{
 
   void startReadTask() async {
     while(true){
-      List<int> bleRead = await this.widget.characteristics.rc.read();
-      String readStr = new String.fromCharCodes(bleRead);
-      String fixedStr = readStr.substring(0,readStr.indexOf(String.fromCharCode(0)));
+      if(running == true){
+        List<int> bleRead = await this.widget.characteristics.rc.read();
+        String readStr = new String.fromCharCodes(bleRead);
+        String fixedStr = readStr.substring(0,readStr.indexOf(String.fromCharCode(0)));
 
-      print("BleRead = "+fixedStr);
+        print("BleRead = "+fixedStr);
 
-      List<String> members = fixedStr.split("|");
+        List<String> members = fixedStr.split("|");
 
-      setState((){
-        lastAdcStr = members[0];
-        lastCalFacStr = members[1];
-        lastMassStr = members[2];
-      });
+        if(members.length < 3){
+          continue;
+        }
+
+        setState((){
+          lastAdcStr = members[0];
+          lastCalFacStr = members[1];
+          lastMassStr = members[2];
+        });
+      }
 
       await new Future.delayed(const Duration(milliseconds : 100));
     }
